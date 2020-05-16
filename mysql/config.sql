737878af1d4fa26ffeb7eadc7ced2c62d55e3036
Morgan <morganitos@hotmail.com>
2020-05-16 19:57:02 +0200

    open mysql to the world !

--- mysql/config.sql
+++ mysql/config.sql
@@ -1,3 +1,5 @@
 CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'newuser'@'%' IDENTIFIED BY 'password';
 CREATE DATABASE db_wordpress;
-GRANT ALL PRIVILEGES ON db_wordpress.* TO 'newuser'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;            --access from outside
--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -4,6 +4,10 @@ rc-update add mariadb default
 
 /etc/init.d/mariadb setup
 chown mysql:mysql /var/lib/mysql
+echo '[mysqld]' >> /etc/my.cnf
+echo 'skip-networking=0' >> /etc/my.cnf
+echo 'skip-bind-address' >> /etc/my.cnf
+
 rc-service mariadb restart
 
 mysql -u root < /etc/config.sql
