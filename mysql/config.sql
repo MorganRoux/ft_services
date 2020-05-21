0366bc471fdcf32dffbb9809065609154c0919c5
Morgan <morganitos@hotmail.com>
2020-05-21 08:50:37 +0200

    mysql my way

--- mysql/config.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
-CREATE USER 'newuser'@'%' IDENTIFIED BY 'password';
-CREATE DATABASE db_wordpress;
-GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;            --access from outside
--- mysql/start.sh
+++ mysql/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 ​
-nohup ./init_db.sh > /dev/null 2>&1 &
-/usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
-/usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+openrc
+touch /run/openrc/softlevel
+rc-update add mariadb default
+
+
+
+#/usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
+/etc/init.d/mariadb setup
+rc-service mariadb restart
+./init_db.sh > /dev/null 2>&1 &
+sleep infinity && wait
