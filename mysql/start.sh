aa59a5071708742a926ef11e120b5612ad2975c8
Morgan <morganitos@hotmail.com>
2020-05-21 08:50:50 +0200

    mysql my way

--- mysql/start.sh
+++ mysql/start.sh
@@ -4,9 +4,6 @@ openrc
 touch /run/openrc/softlevel
 rc-update add mariadb default
 
-
-
-#/usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
 /etc/init.d/mariadb setup
 rc-service mariadb restart
 ./init_db.sh > /dev/null 2>&1 &
